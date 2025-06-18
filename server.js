fb21bb018d88d3b30942a7cb28946c8815d93306
Dani <209375540+DaniAlex187@users.noreply.github.com>
2025-06-18 21:10:13 +0300

    Fix Git Upload

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const db = require('./db');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://bbcleaning-frontend.vercel.app'
+}));
 app.use(express.json());
 
 // Create table if not exists
